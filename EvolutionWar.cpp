7189d86271faa70d0049c531610307427210cdae
StephanieRct <contact@stephanierct.com>
2013-09-18 19:21:12 -0400

    Update EvolutionWar.cpp

--- EvolutionWar.cpp
+++ EvolutionWar.cpp
@@ -9,7 +9,6 @@
 // Use, Copy, Modify, or do whatever you want want with this code. I'm not responsible for anything you do with this and it's not my fault if you die by to much cake eating. :)
 // Also don't mind the ugly code, cheers!
 
-
 #include <cctype>
 #include <map>
 #include <stack>
