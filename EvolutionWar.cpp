300bee7c5f0368eeb7b94ffbd95948f213fff5d1
StephanieRct <contact@stephanierct.com>
2013-09-19 00:45:16 -0400

    Update EvolutionWar.cpp

--- EvolutionWar.cpp
+++ EvolutionWar.cpp
@@ -8,7 +8,9 @@
 // *Result may change from one run to another.
 // Use, Copy, Modify, or do whatever you want want with this code. I'm not responsible for anything you do with this and it's not my fault if you die by to much cake eating. :)
 // Also don't mind the ugly code, cheers!
-
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <vector>
 #include <time.h>
 
